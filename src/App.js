54773476365778f0ea28638fd9d326de1db29865
RekaNagy <99927885+RekaNagy@users.noreply.github.com>
2022-10-29 17:35:03 +0200

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Weather from "./Weather";
+import SourceOfProject from "./SourceOfProject";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="Main">
+        <h1>Weather App</h1>
       <Weather />
+      </div>
+      <div className="SourceOfProject">
+        <SourceOfProject />
+      </div>
     </div>
   );
 }
---
+++ src/SourceOfProject.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SourceOfProject() {
+    return (
+        <p>This project was coded by Réka Nagy and it is 
+            <a href="https://github.com/RekaNagy/react-weather-app" target="_blank" rel="noopener noreferrer"> open-sourced.</a>      
+        </p>
+    )
+}
